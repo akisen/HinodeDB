ec9aceb1afdf6c2c6f15852a5c130a5db5e3abcd
Akito Komatsu <akitrb001@gmail.com>
2020-10-03 16:54:09 +0900

    各年ごとに処理できるようにフロー変更。 各年のデータについて初期化するところまで完成

--- src_dataset/Convert_Coordinate.py
+++ src_dataset/Convert_Coordinate.py
@@ -12,11 +12,11 @@ import utils
 from shapely.geometry import Polygon
 
 YEARS = [2010+i for i in range(10)]
-SOT_SP_PATH = "../sot_sp/SOTSP_*.csv"
-SOT_FG_PATH = "../sot_fg/SOTFG_*.csv"
-EIS_PATH = "../eis/EIS_*.csv"
-XRT_PATH = "../xrt/XRT_*.csv"
-FLARE_PATH = "../flare/Flare2010.csv"
+SOT_SP_PATH = "sot_sp/SOTSP_*.csv"
+SOT_FG_PATH = "sot_fg/SOTFG_*.csv"
+EIS_PATH = "eis/EIS_*.csv"
+XRT_PATH = "xrt/XRT_*.csv"
+FLARE_PATH = "flare/Flare*.csv"
 
 def path_to_dic(path_str):
     paths = sorted(glob.glob(path_str))
@@ -24,7 +24,7 @@ def path_to_dic(path_str):
     return paths_dic
 
 def read_flare_csv(path_str):
-    flare_df = pd.read_csv(FLARE_PATH,index_col=0)
+    flare_df = pd.read_csv(path_str,index_col=0)
     flare_df = flare_df.query('search_observatory=="SDO"')
     flare_df = flare_df.dropna(subset=["fl_goescls"])
     flare_df = flare_df.reset_index(drop=True)
@@ -36,6 +36,11 @@ def add_flare_series(df):
     df["XFlare"] = [ [] for i in range(len(df))]
     return df
 
+def initialize_hinode_df(path):
+    hinode_df = pd.read_csv(path) #TODO:各年度、各データに対して網羅的にデータフレームを作成するように変更
+    hinode_df = add_flare_series(hinode_df)
+    hinode_df = utils.convert_time(hinode_df)
+    return hinode_df
 def line_to_polygon_flare(line):
     ll_x = line["boundbox_c1ll"]
     ll_y = line["boundbox_c2ll"]
@@ -61,34 +66,41 @@ def is_intersection(polygonA,polygonB):
     return polygonA.intersection(polygonB)
 
 def is_in_time (hinode_line,flare_line):
-    hinode_start = hinode_line["DATE_OBS"]
+    hinode_obs = hinode_line["DATE_OBS"]
     hinode_end = hinode_line["DATE_END"]
     flare_start =  datetime.datetime.strptime(flare_line["event_starttime"],"%Y-%m-%dT%H:%M:%S")
     flare_end = datetime.datetime.strptime(flare_line["event_endtime"],"%Y-%m-%dT%H:%M:%S")
-    print(flare_start,hinode_start,hinode_end,flare_end)
-    return flare_start < hinode_start and hinode_end<flare_end
+    print(flare_start,hinode_obs,flare_end)
+    return flare_start <= hinode_obs < flare_end
 
 def main():
     sot_sp_paths_dic = path_to_dic(SOT_SP_PATH)#各年度でPathを格納した辞書を作成
     sot_fg_paths_dic = path_to_dic(SOT_FG_PATH)
     eis_paths_dic = path_to_dic(EIS_PATH)
     xrt_path_dic =path_to_dic(XRT_PATH)
-    flare_df = read_flare_csv(FLARE_PATH)
-    sot_fg_2010 = pd.read_csv(sot_fg_paths_dic["2010"])
-    sot_fg_2010 = add_flare_series(sot_fg_2010)
-    sot_fg_2010 = utils.convert_time(sot_fg_2010)
-    flare_polygon =line_to_polygon_flare(flare_df.iloc[0])
-    hinode_polygon = line_to_polygon_hinode(sot_fg_2010.loc[0])
-    for i in range(len(flare_df)):
-        flare_line = flare_df.loc[i]
-        flare_polygon = line_to_polygon_flare(flare_line)
-        # print("flare:"+str(flare_polygon))
-        for j in range(len(sot_fg_2010)):
-            hinode_line = sot_fg_2010.loc[j]
-            hinode_polygon = line_to_polygon_hinode(hinode_line)
-            print(is_in_time(hinode_line,flare_line))
-            if is_in_time(hinode_line,flare_line) and is_intersection(hinode_polygon,flare_polygon):
-                print("intersection")
+    hinode_dics = [sot_sp_paths_dic,sot_fg_paths_dic,eis_paths_dic,xrt_path_dic]
+    flare_path_dic = path_to_dic(FLARE_PATH)
+    for year in YEARS:
+        print(year)
+        for hinode_dic in hinode_dics:
+            if (hinode_dic.__len__()==7 and year > 2016): #sot_fgのデータが2016年分までしかないため
+                continue
+            else:
+                hinode_df = initialize_hinode_df(hinode_dic[str(year)])    
+                print(hinode_df)
+        exit()
+    #     flare_df = read_flare_csv(flare_path_dic[str(year)])
+    #     exit()
+    # for i in range(len(flare_df)):
+    #     flare_line = flare_df.loc[i]
+    #     flare_polygon = line_to_polygon_flare(flare_line)
+    #     # print("flare:"+str(flare_polygon))
+    #     for j in range(len(sot_df)):
+    #         hinode_line = sot_df.loc[j]
+    #         hinode_polygon = line_to_polygon_hinode(hinode_line)
+    #         print(is_in_time(hinode_line,flare_line))
+    #         if is_in_time(hinode_line,flare_line) and is_intersection(hinode_polygon,flare_polygon):
+    #             print("intersection")
 
 
 main()
