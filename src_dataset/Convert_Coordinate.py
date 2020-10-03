b263b1c3e3125156aabc4df1c6949f94304dd04c
Akito Komatsu <akitrb001@gmail.com>
2020-10-03 20:14:32 +0900

    一応仮で書き出しまで書いた。 ただMap関数の扱いに若干自信がないのでもう少し勉強が必要

--- src_dataset/Convert_Coordinate.py
+++ src_dataset/Convert_Coordinate.py
@@ -31,6 +31,7 @@ def read_flare_csv(path_str):
     return flare_df
 
 def add_flare_series(df):
+    df["BFlare"] = [ [] for i in range(len(df))]
     df["CFlare"] = [ [] for i in range(len(df))]
     df["MFlare"] = [ [] for i in range(len(df))]
     df["XFlare"] = [ [] for i in range(len(df))]
@@ -87,7 +88,32 @@ def is_in_time (hinode_line,flare_line):
     return flare_start <= hinode_obs < flare_end
 
 def add_flare_label(hinode_line,flare_line):
-    pass
+    flare_class = flare_line.fl_goescls[0]
+    flare_label = "{}:{}".format(flare_line.SOL_standard,flare_line.fl_goescls)
+    if(flare_class == "B"):
+        hinode_line.BFlare.append(flare_label)
+        print("B:{}".format(flare_label))
+    elif(flare_class == "C"):
+        hinode_line.CFlare.append(flare_label)
+        print("C:{}".format(flare_label))
+    elif(flare_class == "M"):
+        hinode_line.MFlare.append(flare_label)
+        print("M:{}".format(flare_label))
+    elif(flare_class == "`X"):
+        hinode_line.XFlare.append(flare_label)
+        print("X:{}".format(flare_label))
+
+def write_log(hinode_line,flare_line):
+    tqdm.write("intersection\nflare_time{}-{}\nflare_point:{}\nhinode_time{}\nhinode_polygon:XCEN{},YCEN{},FOVX{},FOVY{}".format(flare_line.event_starttime,flare_line.event_endtime,flare_line.hpc_coord,hinode_line.DATE_OBS,hinode_line.XCEN,hinode_line.YCEN,hinode_line.FOVX,hinode_line.FOVY))
+
+def export_csv(hinode_df,old_path):
+    hinode_df["BFlare"].map("   ".join({}))#リストのままだとCSVに書き出しできないのでタブ区切りに変換
+    hinode_df["CFlare"].map("   ".join({}))
+    hinode_df["MFlare"].map("   ".join({}))
+    hinode_df["XFlare"].map("   ".join({}))
+    new_path = "flare_labeled/{}".format(old_path.split("/")[-1])
+    hinode_df.to_csv(hinode_df,new_path)
+
 def main():
     sot_sp_paths_dic = path_to_dic(SOT_SP_PATH)#各年度でPathを格納した辞書を作成
     sot_fg_paths_dic = path_to_dic(SOT_FG_PATH)
@@ -103,21 +129,25 @@ def main():
                 continue
             else:
                 hinode_df = initialize_hinode_df(hinode_dic[str(year)])
-                pbar = tqdm(total = len(flare_df))
-                for flare_line in flare_df.itertuples():
-                    flare_point = line_to_point_flare(flare_line)
-                    # print(point)
-                    # flare_polygon = line_to_polygon_flare(flare_line)
-                    # print(flare_polygon)
-                    for hinode_line in hinode_df.itertuples():
-                        hinode_polygon = line_to_polygon_hinode(hinode_line)
-                        if is_in_time(hinode_line,flare_line) and is_contained(hinode_polygon,flare_point):
-                            tqdm.write("intersection\nflare_time{}-{}\nflare_point:{}\nhinode_time{}\nhinode_polygon:XCEN{},YCEN{},FOVX{},FOVY{}".format(flare_line.event_starttime,flare_line.event_endtime,flare_line.hpc_coord,hinode_line.DATE_OBS,hinode_line.XCEN,hinode_line.YCEN,hinode_line.FOVX,hinode_line.FOVY))
-                        else:
-                            pass
-                            # print("not intersection")
-                    pbar.update(1)
-                pbar.close()
+                with tqdm(total = len(flare_df)) as pbar:
+                    for flare_line in flare_df.itertuples():
+                        flare_point = line_to_point_flare(flare_line)
+                        # print(flare_line)
+                        # print(point)
+                        # flare_polygon = line_to_polygon_flare(flare_line)
+                        # print(flare_polygon)
+                        for hinode_line in hinode_df.itertuples():
+                            hinode_polygon = line_to_polygon_hinode(hinode_line)
+                            if is_in_time(hinode_line,flare_line) and is_contained(hinode_polygon,flare_point):
+                                add_flare_label(hinode_line,flare_line)
+                                write_log(hinode_line,flare_line)
+                            else:
+                                pass
+                                # print("not intersection")
+                        # print(hinode_df.CFlare)
+                        pbar.update(1)
+                export_csv(hinode_df,hinode_dic[str(year)])
+                
 
 
 main()
