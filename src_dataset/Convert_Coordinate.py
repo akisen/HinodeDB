d0e7f5e1b7ba70bedc315acd5ea97aa3252e5052
Akito Komatsu <akitrb001@gmail.com>
2021-08-19 17:55:36 +0900

    整形

--- src_dataset/Convert_Coordinate.py
+++ src_dataset/Convert_Coordinate.py
@@ -40,95 +40,111 @@ EIS_PATH = "eis/EIS_*.csv"
 XRT_PATH = "xrt/XRT_*.csv"
 FLARE_PATH = "FL/SOL_all_FL*.csv"
 
+
 def main():
-    sot_sp_paths_dic = path_to_dic(SOT_SP_PATH)#各年度でPathを格納した辞書を作成
+    sot_sp_paths_dic = path_to_dic(SOT_SP_PATH)  # 各年度でPathを格納した辞書を作成
     sot_fg_paths_dic = path_to_dic(SOT_FG_PATH)
     eis_paths_dic = path_to_dic(EIS_PATH)
-    xrt_path_dic =path_to_dic(XRT_PATH)
-    hinode_dics = [sot_sp_paths_dic,sot_fg_paths_dic,eis_paths_dic,xrt_path_dic]
+    xrt_path_dic = path_to_dic(XRT_PATH)
+    hinode_dics = [sot_sp_paths_dic,
+                   sot_fg_paths_dic, eis_paths_dic, xrt_path_dic]
     flare_path_dic = path_to_dic(FLARE_PATH)
     for year in YEARS:
         flare_df = read_flare_csv(flare_path_dic[str(year)])
         for hinode_dic in hinode_dics:
-            if (hinode_dic.__len__()==7 and year > 2016): #sot_fgのデータが2016年分までしかないため
+            if (hinode_dic.__len__() == 7 and year > 2016):  # sot_fgのデータが2016年分までしかないため
                 continue
             else:
                 hinode_df = initialize_hinode_df(hinode_dic[str(year)])
-                with tqdm(total = len(flare_df),desc="{}".format(hinode_dic[str(year)])) as pbar:
+                with tqdm(total=len(flare_df), desc="{}".format(hinode_dic[str(year)])) as pbar:
                     for flare_row in flare_df.itertuples():
                         flare_point = line_to_point_flare(flare_row)
                         for hinode_row in hinode_df.itertuples():
                             hinode_polygon = line_to_polygon_hinode(hinode_row)
-                            if is_in_time(hinode_row,flare_row) and is_contained(hinode_polygon,flare_point):
-                                add_flare_label(hinode_row,flare_row)
-                                write_log(hinode_row,flare_row,hinode_dic[str(year)])
+                            if is_in_time(hinode_row, flare_row) and is_contained(hinode_polygon, flare_point):
+                                add_flare_label(hinode_row, flare_row)
+                                write_log(hinode_row, flare_row,
+                                          hinode_dic[str(year)])
                         pbar.update(1)
                 # utils.pickle_dump(hinode_df,"pickles/{}pickle".format(hinode_dic[str(year)].split("/")[-1][:-3]))
-                export_csv(hinode_df,hinode_dic[str(year)])
-                
+                export_csv(hinode_df, hinode_dic[str(year)])
+
+
 def path_to_dic(path_str):
     paths = sorted(glob.glob(path_str))
-    paths_dic = {path.split("/")[-1][-8:-4]:path for path in paths}
+    paths_dic = {path.split("/")[-1][-8:-4]: path for path in paths}
     return paths_dic
 
+
 def read_flare_csv(path_str):
-    flare_df = pd.read_csv(path_str,index_col=0)
-    flare_df = flare_df.drop(flare_df.index[flare_df["boundbox_c1ll"]<=-90])# 観測座標がおかしい行の削除
-    flare_df = flare_df.drop_duplicates() # 重複行の削除
-    flare_df = flare_df.dropna(subset=["fl_goescls"]) #GOESクラスが入っていない行を削除
+    flare_df = pd.read_csv(path_str, index_col=0)
+    flare_df = flare_df.drop(
+        flare_df.index[flare_df["boundbox_c1ll"] <= -90])  # 観測座標がおかしい行の削除
+    flare_df = flare_df.drop_duplicates()  # 重複行の削除
+    flare_df = flare_df.dropna(subset=["fl_goescls"])  # GOESクラスが入っていない行を削除
     flare_df = flare_df.reset_index(drop=True)
     return flare_df
 
+
 def add_flare_series(df):
-    df["BFlare"] = [ [] for i in range(len(df))]
-    df["CFlare"] = [ [] for i in range(len(df))]
-    df["MFlare"] = [ [] for i in range(len(df))]
-    df["XFlare"] = [ [] for i in range(len(df))]
+    df["BFlare"] = [[] for i in range(len(df))]
+    df["CFlare"] = [[] for i in range(len(df))]
+    df["MFlare"] = [[] for i in range(len(df))]
+    df["XFlare"] = [[] for i in range(len(df))]
     return df
+
 
 def initialize_hinode_df(path):
     hinode_df = pd.read_csv(path)
-    hinode_df = hinode_df.dropna(axis=0,how="any")
+    hinode_df = hinode_df.dropna(axis=0, how="any")
     hinode_df = hinode_df.reset_index(drop=True)
     hinode_df = add_flare_series(hinode_df)
     hinode_df = utils.convert_time(hinode_df)
     return hinode_df
 
+
 def line_to_point_flare(line):
     point = line.hpc_coord[6:-1].split(" ")
-    point = [float(p) for p in point]#Int型に型変換
+    point = [float(p) for p in point]  # Int型に型変換
     point = Point(point)
     return point
+
 
 def line_to_polygon_flare(line):
     ll_x = line.boundbox_c1ll
     ll_y = line.boundbox_c2ll
     ur_x = line.boundbox_c1ur
     ur_y = line.boundbox_c2ur
-    polygon = Polygon([(ll_x,ll_y),(ll_x,ur_y),(ur_x,ur_y),(ur_x,ll_y)])
+    polygon = Polygon([(ll_x, ll_y), (ll_x, ur_y), (ur_x, ur_y), (ur_x, ll_y)])
     return polygon
 
-def line_to_polygon_hinode (line):
+
+def line_to_polygon_hinode(line):
     ll_x = line.XCEN-(line.FOVX//2)
     ur_x = line.XCEN+(line.FOVX//2)
     ll_y = line.YCEN-(line.FOVY//2)
     ur_y = line.YCEN+(line.FOVY//2)
-    polygon = Polygon([(ll_x,ll_y),(ll_x,ur_y),(ur_x,ur_y),(ur_x,ll_y)])
+    polygon = Polygon([(ll_x, ll_y), (ll_x, ur_y), (ur_x, ur_y), (ur_x, ll_y)])
     return polygon
 
-def is_contained(polygon,point):
+
+def is_contained(polygon, point):
     return polygon.contains(point)
 
-def is_in_time (hinode_row,flare_row):
+
+def is_in_time(hinode_row, flare_row):
     hinode_obs = hinode_row.DATE_OBS
     hinode_end = hinode_row.DATE_END
-    flare_start =  datetime.datetime.strptime(flare_row.event_starttime,"%Y-%m-%dT%H:%M:%S")
-    flare_end = datetime.datetime.strptime(flare_row.event_endtime,"%Y-%m-%dT%H:%M:%S")
+    flare_start = datetime.datetime.strptime(
+        flare_row.event_starttime, "%Y-%m-%dT%H:%M:%S")
+    flare_end = datetime.datetime.strptime(
+        flare_row.event_endtime, "%Y-%m-%dT%H:%M:%S")
     return flare_start <= hinode_obs < flare_end
 
-def add_flare_label(hinode_row,flare_row):
+
+def add_flare_label(hinode_row, flare_row):
     flare_class = flare_row.fl_goescls[0]
-    flare_label = "{}:{}".format(flare_row.SOL_standard,flare_row.fl_goescls)
+    flare_label = "{}:{}".format(flare_row.SOL_standard, flare_row.fl_goescls)
     if(flare_class == "B"):
         hinode_row.BFlare.append(flare_label)
         tqdm.write("B:{}".format(flare_label))
@@ -142,22 +158,24 @@ def add_flare_label(hinode_row,flare_row):
         hinode_row.XFlare.append(flare_label)
         tqdm.write("X:{}".format(flare_label))
 
-def write_log(hinode_row,flare_row,hinode_path):
-    logtext = "intersection\nflare_time{}-{}\nflare_point:{}\nhinode_time{}\nhinode_polygon:XCEN{},YCEN{},FOVX{},FOVY{}".format(flare_row.event_starttime,flare_row.event_endtime,flare_row.hpc_coord,hinode_row.DATE_OBS,hinode_row.XCEN,hinode_row.YCEN,hinode_row.FOVX,hinode_row.FOVY)
+
+def write_log(hinode_row, flare_row, hinode_path):
+    logtext = "intersection\nflare_time{}-{}\nflare_point:{}\nhinode_time{}\nhinode_polygon:XCEN{},YCEN{},FOVX{},FOVY{}".format(
+        flare_row.event_starttime, flare_row.event_endtime, flare_row.hpc_coord, hinode_row.DATE_OBS, hinode_row.XCEN, hinode_row.YCEN, hinode_row.FOVX, hinode_row.FOVY)
     tqdm.write(logtext)
     logpath = "logs/log_{}txt".format(hinode_path.split("/")[-1][:-3])
-    with open (logpath,"a") as f:
-        print(logtext,file = f)
+    with open(logpath, "a") as f:
+        print(logtext, file=f)
 
-def export_csv(hinode_df,old_path):
-    hinode_df["BFlare"] = hinode_df["BFlare"].map(lambda x:"   ".join(x))#リストのままだとCSVに書き出しできないのでタブ区切りに変換
-    hinode_df["CFlare"] = hinode_df["CFlare"].map(lambda x:"   ".join(x))
-    hinode_df["MFlare"] = hinode_df["MFlare"].map(lambda x:"   ".join(x))
-    hinode_df["XFlare"] = hinode_df["XFlare"].map(lambda x:"   ".join(x))
+
+def export_csv(hinode_df, old_path):
+    hinode_df["BFlare"] = hinode_df["BFlare"].map(
+        lambda x: "   ".join(x))  # リストのままだとCSVに書き出しできないのでタブ区切りに変換
+    hinode_df["CFlare"] = hinode_df["CFlare"].map(lambda x: "   ".join(x))
+    hinode_df["MFlare"] = hinode_df["MFlare"].map(lambda x: "   ".join(x))
+    hinode_df["XFlare"] = hinode_df["XFlare"].map(lambda x: "   ".join(x))
     new_path = "flare_labeled/{}".format(old_path.split("/")[-1])
     hinode_df.to_csv(new_path)
-
-
 
 
 main()
